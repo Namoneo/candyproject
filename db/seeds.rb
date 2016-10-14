1ec6e63ced7bf1c825624e5bcbdb88c8ad2c7413
MalenkavanOmmen <malenkavanommen@gmail.com>
2016-10-14 13:11:02 +0200

    Add first test profiles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Profile.delete_all
+User.delete_all
+
+malenka = User.create(email: "malenka@codaisseurbnb.com", password: "secret")
+sinterklaas=User.create(email: "sinterklaas@codaisseurbnb.com", password: "secret")
+
+
+malenka_profile = Profile.create(first_name: "Malenka", last_name: "van Ommen", street_address: "Blablabla", number: "42", city: "Arnhem", zip_code: "1234ab", state_region: "Gelderland", country: "Netherland")
+sinterklaas_profile = Profile.create(first_name: "Sint", last_name: "Nicolaas", street_address: "Sintstreet", number: 1, city: "Madrid", zip_code: "5678er", state_region: "Sintplace", country: "Spain")
+
+
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+  describe "validations" do
+    it "is invalid without a first_name" do
+      profile = Profile.new(first_name: "")
+      profile.valid?
+      expect(profile.errors).to have_key(:first_name)
+    end
+
+    it "is invalid without a last_name" do
+      profile = Profile.new(last_name: "")
+      profile.valid?
+      expect(profile.errors).to have_key(:last_name)
+    end
+
+    it "is invalid without a street_address" do
+      profile = Profile.new(street_address: "")
+      profile.valid?
+      expect(profile.errors).to have_key(:street_address)
+    end
+
+    it "is invalid without a number" do
+      profile = Profile.new(number: "")
+      profile.valid?
+      expect(profile.errors).to have_key(:number)
+    end
+
+    it "is invalid without a city" do
+      profile = Profile.new(city: "")
+      profile.valid?
+      expect(profile.errors).to have_key(:city)
+    end
+
+    it "is invalid without a zip_code" do
+      profile = Profile.new(zip_code: "")
+      profile.valid?
+      expect(profile.errors).to have_key(:zip_code)
+    end
+
+    it "is invalid without a state_region" do
+      profile = Profile.new(state_region: "")
+      profile.valid?
+      expect(profile.errors).to have_key(:state_region)
+    end
+
+    it "is invalid without a country" do
+      profile = Profile.new(country: "")
+      profile.valid?
+      expect(profile.errors).to have_key(:country)
+    end
+
+    describe User do
+      describe "#full_name" do
+        it "is composed of first and last name" do
+          profile = build(:profile, first_name: "Malenka", last_name: "van Ommen")
+          expect(profile.full_name).to eq "Malenka van Ommen"
+        end
+      end
+    end
+  end
 end
