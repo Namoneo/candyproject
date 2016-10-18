374df1b7973cb15e527a0c76c7464e344088dade
Konstantinos <konstantinos.gkogkoglou@gmail.com>
2016-10-18 15:10:22 +0200

    cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,11 +2,27 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :photos, dependent: :destroy
   belongs_to :category
+  has_many :line_items
 
   validates :name, presence: true
   validates :price, presence: true
 
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+
   def self.order_by_name
     order(:name)
   end
+
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
+
+
 end
